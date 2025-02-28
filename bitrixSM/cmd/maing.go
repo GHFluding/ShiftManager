f374cb3033780aa2eced6b9b729c2cd6827bf352
GHFluding <spartacocgovaho@gmail.com>
2025-02-28 18:52:34 +0300

    b24

--- bitrixSM/cmd/maing.go
+++ bitrixSM/cmd/maing.go
@@ -6,21 +6,20 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-// TODO: inti kafka
 func main() {
-	// Конфигурация
+	//TODO: init logger
 	webhookURL := "https://your-bitrix-portal.bitrix24.ru/rest/1/WEBHOOK_TOKEN"
 	secret := "your_webhook_secret"
 
-	// Инициализация клиента
 	bitrixClient := bitrix.NewClient(webhookURL)
 	botHandler := bot.NewBotHandler(bitrixClient, secret)
 
-	// Настройка роутера
+	// TODO: inti kafka
+
+	//TODO: rework
 	r := gin.Default()
 	r.POST("/bitrix/webhook", botHandler.WebhookHandler)
 
-	// Запуск сервера
 	if err := r.Run(":8080"); err != nil {
 		panic(fmt.Sprintf("Failed to start server: %v", err))
 	}
