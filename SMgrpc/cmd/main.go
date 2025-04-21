e667fd3858a787399325a36a51b5af0b40acb0de
GHFluding <spartacocgovaho@gmail.com>
2025-04-21 13:32:51 +0300

    added gracefull stop and gorutines

--- SMgrpc/cmd/main.go
+++ SMgrpc/cmd/main.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"log/slog"
+	"os"
+	"os/signal"
 	"smgrpc/internal/app"
 	grpcapp "smgrpc/internal/app/grpc"
 	"smgrpc/internal/config"
 	sl "smgrpc/internal/utils/logger"
+	"syscall"
 )
 
 func main() {
@@ -18,11 +21,24 @@ func main() {
 	)
 
 	machineApplication := app.New(log, cfg.GRPC.Port, grpcapp.MachineServer)
-	machineApplication.GRPCServer.MustRun()
-	taskApplication := app.New(log, cfg.GRPC.Port, grpcapp.TaskServer)
-	taskApplication.GRPCServer.MustRun()
-	userApplication := app.New(log, cfg.GRPC.Port, grpcapp.UserServer)
-	userApplication.GRPCServer.MustRun()
-	shiftApplication := app.New(log, cfg.GRPC.Port, grpcapp.ShiftServer)
-	shiftApplication.GRPCServer.MustRun()
+	taskApplication := app.New(log, cfg.GRPC.Port+1, grpcapp.TaskServer)
+	userApplication := app.New(log, cfg.GRPC.Port+2, grpcapp.UserServer)
+	shiftApplication := app.New(log, cfg.GRPC.Port+3, grpcapp.ShiftServer)
+
+	go machineApplication.GRPCServer.MustRun()
+	go shiftApplication.GRPCServer.MustRun()
+	go userApplication.GRPCServer.MustRun()
+	go taskApplication.GRPCServer.MustRun()
+
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, syscall.SIGTERM, syscall.SIGINT)
+
+	<-stop
+
+	machineApplication.GRPCServer.Stop()
+	taskApplication.GRPCServer.Stop()
+	userApplication.GRPCServer.Stop()
+	shiftApplication.GRPCServer.Stop()
+
+	log.Info("Gracefully stopped")
 }
