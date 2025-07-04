fcc58fc1ca4fd9191edf358db0a03f6dc578d458
GHFluding <spartacocgovaho@gmail.com>
2025-07-04 13:56:51 +0300

    added initialization routers

---
+++ telegrambot/cmd/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"context"
+	"log"
+	"os"
+	"telegramSM/internal/telegramapi/commands"
+	"telegramSM/internal/telegramapi/model"
+
+	tgBotAPI "github.com/go-telegram-bot-api/telegram-bot-api"
+)
+
+func main() {
+	//TODO: init config
+	botToken := os.Getenv("TELEGRAM_BOT_TOKEN")
+	bot, err := tgBotAPI.NewBotAPI(botToken)
+	if err != nil {
+		log.Panicf("Ошибка инициализации бота: %v", err)
+	}
+	bot.Debug = true
+
+	router := commands.NewRouter()
+	//TODO: init interfaces
+
+	router.RegisterCommandHandler(model.CmdStart, commands.StartHandler(userService))
+	router.RegisterCommandHandler(model.CmdHelp, commands.HelpHandler(userService))
+	router.RegisterCommandHandler(model.CmdCreateTask, commands.CreateTaskHandler(taskService, machineService))
+
+	//TODO: refactor callback register function
+	router.RegisterMessageHandler(commands.SkipBitrixHandler(userService))
+	router.RegisterMessageHandler(commands.TaskCallbackHandler(machineService, shiftService, taskService))
+
+	u := tgBotAPI.NewUpdate(0)
+	u.Timeout = 60
+	updates, err := bot.GetUpdatesChan(u)
+	if err != nil {
+		log.Panicf("Error getting update: %v", err)
+	}
+
+	ctx := context.Background()
+	for update := range updates {
+		if err := router.HandleUpdate(ctx, bot, update); err != nil {
+			log.Printf("Error handle update: %v", err)
+		}
+	}
+}
